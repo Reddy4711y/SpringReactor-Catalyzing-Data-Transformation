9404190e0e36c20cefcf3eb1be37157792125c0a
Reddy4711y <rr6521712@gmail.com>
2023-10-07 17:23:52 +0530

    Full stack app created successfully

---
+++ SpringReactorBackend/src/main/java/com/demo/backend/BackendAppCrudApplication.java
@@ -0,0 +1,13 @@
+package com.demo.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendAppCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendAppCrudApplication.class, args);
+	}
+
+}
---
+++ SpringReactorFrontend/src/pages/Footer.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Footer() {
+  return (
+    <div className="footer text-primary">
+      <h5>Created by @Hemanth Reddy</h5>
+    </div>
+  );
+}
